8926eeb1867c7cf7609808fcf1e47a5487614b3e
Mahendran, Rupesh Kumar <mrk@gatech.edu>
2022-11-28 18:51:07 -0500

    Update run_files.sh

--- run_files.sh
+++ run_files.sh
@@ -6,6 +6,7 @@ for graph in ${graphFiles}
 do
 	echo ${graph}
 	python ./src/main.py -inst ${graph} -alg Approx -time 20 -seed 10
+	python ./src/main.py -inst ${graph} -alg LS1 -time 20 -seed 10	
 	python ./src/main.py -inst ${graph} -alg LS2 -time 20 -seed 10
 done
 
