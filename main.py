46a090caf5a2615d803dd00b9b2807619f9b495b
Pranoy Ray <80657118+pranoy-ray@users.noreply.github.com>
2022-11-28 16:54:18 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+import os
+import time
+import argparse
+import networkx as nx
+from Approx import Approx
+from ls2 import Hill
+
+parser = argparse.ArgumentParser(description='Find Minimum Vertex Cover (MVC)')
+parser.add_argument('-inst', help='Graph Instance')
+parser.add_argument('-alg', help='Algorithm for finding the MVC')
+parser.add_argument('-time', help='Cutoff Time (in secs)')
+parser.add_argument('-seed', help='Random Seed')
+args = parser.parse_args()
+
+# Add the corresponding path for accessing the graph data files
+data_path = '/Users/pranoy/Dropbox (GaTech)/Research/Classes/CSE6140-ALGO/Project/DATA/'
+output_path= '/Users/pranoy/Dropbox (GaTech)/Research/Classes/CSE6140-ALGO/Project/output/'
+
+def create_graph(file_name,VE=False):
+
+    file_path = data_path + file_name
+    graph_file = open(file_path, 'r')
+    graph_data = graph_file.readlines()
+    n_vertices, n_edges, is_weighted = graph_data[0].split()
+
+    G = nx.Graph()
+
+    for i, neighbours in enumerate(graph_data):
+        if i != 0:
+            neighbour = neighbours.split()
+            for v in neighbour:
+                G.add_edge(int(i), int(v))
+                
+    with open(file_path, 'r') as vertices:
+        V, E, Temp = vertices.readline().split()
+        return G, int(V), int(E)
+
+if __name__=="__main__":
+
+    G, V, E = create_graph(args.inst)
+    graph_file = args.inst
+    cutoff = args.time
+    randSeed = args.seed
+    # print(G.edges(), G.nodes())
+
+    if(args.alg == "BnB"):
+        # Function call to the BnB Function
+        pass
+
+    elif(args.alg == "Approx"):
+        # Function call to the Approx Function
+        sol, trace = Approx(G)
+
+    elif(args.alg == "LS1"):
+        # Function call to the LS1 Function
+        pass
+
+    elif(args.alg == "LS2"):
+        # Function call to the LS2 Function
+        sol, trace = Hill(G,V,E,randSeed,cutoff)
+        #pass
+
+    else:
+        print('-- Algo type missing --')
+        exit()
+
+
+    # Creating the solution and trace files for storing the data
+    if(args.alg == "Approx" or args.alg=="BnB"):
+        sol_file = output_path + graph_file[0:-6] + '_' + args.alg + '_' + cutoff + '.sol'
+        trace_file = output_path + graph_file[0:-6] + '_' + args.alg + '_' + cutoff + '.trace'
+    else:
+        sol_file = output_path + graph_file[0:-6] + '_' + args.alg + '_' + cutoff + '_' + randSeed + '.sol'
+        trace_file = output_path + graph_file[0:-6] + '_' + args.alg + '_' + cutoff + '_' + randSeed + '.trace'   
+
+    f = open(sol_file, 'w')
+    f.write(sol)
+    f.close()
+
+    f = open(trace_file, 'w')
+    f.write(trace)
+    f.close()
