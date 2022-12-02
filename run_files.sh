0acc4bf6e1cb5aa572eb505656f720116476b820
Anahitaa-R <87931153+Anahitaa-R@users.noreply.github.com>
2022-12-03 01:26:17 +0530

    Update run_files.sh

--- run_files.sh
+++ run_files.sh
@@ -4,12 +4,15 @@ graphFiles=`ls ./data/ | grep .graph`
 
 for graph in ${graphFiles}
 do
+	RANDOM=$(date +%s)
 	echo ${graph}
-	python ./code/main.py -inst ${graph} -alg Approx -time 600 -seed 10
+	echo  ${RANDOM}
+	
+	python ./code/main.py -inst ${graph} -alg Approx -time 600 -seed ${RANDOM}
 	for t in {20..100..20}
 		do
-			python ./code/main.py -inst ${graph} -alg LS1 -time ${t} -seed 10	
-			python ./code/main.py -inst ${graph} -alg LS2 -time ${t} -seed 10
+			python ./code/main.py -inst ${graph} -alg LS1 -time ${t} -seed ${RANDOM}	
+			python ./code/main.py -inst ${graph} -alg LS2 -time ${t} -seed ${RANDOM}
 		done
 	python ./code/main.py -inst ${graph} -alg bnb -time 600
 done
