97c26d478608a9dade96952be4429b7ccd8108ca
Anahitaa-R <87931153+Anahitaa-R@users.noreply.github.com>
2022-12-03 01:20:56 +0530

    Update run_files.sh

--- run_files.sh
+++ run_files.sh
@@ -6,7 +6,7 @@ for graph in ${graphFiles}
 do
 	echo ${graph}
 	python ./Code/main.py -inst ${graph} -alg Approx -time 600 -seed 10
-	for t in {20..100..20}
+	for t in {20..101..20}
 		do
 			python ./Code/main.py -inst ${graph} -alg LS1 -time 20 -seed 10	
 			python ./Code/main.py -inst ${graph} -alg LS2 -time 20 -seed 10
